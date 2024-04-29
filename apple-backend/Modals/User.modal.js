e67681444389cbd6cfd6ba8e2b959ee38c1c5d7e
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2024-04-30 03:08:05 +0530

    update

--- apple-backend/Modals/User.modal.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const User = new Schema({
-    name : String,
-    email : String,
-    password : String,
-})
-
-export default mongoose.model("users",User)
